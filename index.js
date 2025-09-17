0a1a69409caf7116f6745109840edda12aa7865a
Caiti Lange <clange441@gmail.com>
2025-09-17 14:15:33 -0500

    added css and created some base code for js

---
+++ index.js
@@ -0,0 +1,21 @@
+const flavors = {
+    vanilla: 3,
+    chocolate: 3,
+    strawberry: 4,
+    coffee: 4,
+    cookies_n_cream: 5,
+    cookie_dough: 5,
+};
+
+
+function 
+
+
+
+const userInputString = prompt(
+  "Please enter enter a list of comma-separated froyo flavors.",
+  "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
+);
+
+const numbers = convertStringToNumbers(userInputString);
+describeNumbers(numbers);
